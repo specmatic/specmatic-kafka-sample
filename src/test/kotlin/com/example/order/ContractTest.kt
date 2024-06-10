6f66a02dae6df51279c5cf37cf4d1c5b36965f6f
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-10 16:51:06 +0530

    Move externalised examples to specmatic directory

--- src/test/kotlin/com/example/order/ContractTest.kt
+++ src/test/kotlin/com/example/order/ContractTest.kt
@@ -2,7 +2,7 @@ package com.example.order
 
 import com.example.order.testcontainers.KafkaTestContainer
 import `in`.specmatic.async.junit.SpecmaticKafkaContractTest
-import `in`.specmatic.async.utils.EXTERNALISED_EXAMPLE_DIR
+import `in`.specmatic.async.utils.EXAMPLES_DIR
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.SpringApplication
@@ -17,7 +17,7 @@ class ContractTest : SpecmaticKafkaContractTest {
         @JvmStatic
         @BeforeAll
         fun setup() {
-            System.setProperty(EXTERNALISED_EXAMPLE_DIR, "src/test/resources")
+            System.setProperty(EXAMPLES_DIR, "src/test/resources")
             kafkaBroker.start()
 
             context = SpringApplication.run(OrderServiceApplication::class.java)
