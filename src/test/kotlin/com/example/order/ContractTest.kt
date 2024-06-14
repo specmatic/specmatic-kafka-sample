5be539c53bed1a11c65c71073c8e5aee5f537778
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-14 10:45:10 +0530

    Add a note in ContractTest

--- src/test/kotlin/com/example/order/ContractTest.kt
+++ src/test/kotlin/com/example/order/ContractTest.kt
@@ -19,6 +19,7 @@ class ContractTest : SpecmaticKafkaContractTest {
         @BeforeAll
         fun setup() {
             System.setProperty(EXAMPLES_DIR, "src/test/resources")
+            // NOTE - cannot get it from application.properties using @Value since @Value does not work in a companion object, hence hardcoding
             System.setProperty(CONSUMER_GROUP_ID, "order-consumer-group-id")
             kafkaBroker.start()
 
