4bac60a1dafb193b099286d6db9e65b4a2f82790
Joel Rosario <cirquitz@gmail.com>
2024-12-05 12:17:27 +0100

    Got updated 2.6 spec contract tests passing

--- src/main/kotlin/com/example/order/OrderService.kt
+++ src/main/kotlin/com/example/order/OrderService.kt
@@ -2,6 +2,7 @@ package com.example.order
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.Acknowledgment
@@ -9,8 +10,11 @@ import org.springframework.stereotype.Service
 import java.math.BigDecimal
 
 private const val ORDER_STATUS_PROCESSED = "PROCESSED"
-private const val ORDER_STATUS_CANCELLED = "CANCELLED"
+private const val CANCELLATION_COMPLETED = "COMPLETED"
+
 private const val NOTIFICATION_TYPE_ORDER_PLACED = "ORDER_PLACED"
+private const val NOTIFICATION_TYPE_ORDER_CANCELLED = "ORDER_CANCELLED"
+
 private const val SERVICE_NAME = "order-service"
 
 @Service
@@ -25,27 +29,71 @@ class OrderService(
     companion object {
         private const val PLACE_ORDER_TOPIC = "place-order"
         private const val PROCESS_ORDER_TOPIC = "process-order"
+        private const val CANCEL_ORDER_TOPIC = "cancel-order"
+        private const val PROCESS_CANCELLATION_TOPIC = "process-cancellation"
         private const val NOTIFICATION_TOPIC = "notification"
     }
 
-    @KafkaListener(topics = [PLACE_ORDER_TOPIC])
-    fun run(orderRequest: String, ack: Acknowledgment) {
-        println("[$SERVICE_NAME] Received message on topic $PLACE_ORDER_TOPIC - $orderRequest")
+    @KafkaListener(topics = [PLACE_ORDER_TOPIC, CANCEL_ORDER_TOPIC])
+    fun placeOrder(record: ConsumerRecord<String, String>, ack: Acknowledgment) {
+        val topic = record.topic()
 
-        val orderRequestJson = try {
-            jacksonObjectMapper().apply {
-                configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
-            }.readValue(orderRequest, OrderRequest::class.java)
-        } catch(e: Exception) {
-            throw e
+        when(topic) {
+            PLACE_ORDER_TOPIC -> {
+                val orderRequest = record.value()
+                val headers = record.headers()
+
+                println("[$SERVICE_NAME] Received message on topic $PLACE_ORDER_TOPIC - $orderRequest")
+                println("[$SERVICE_NAME] Headers: ${headers.joinToString { "${it.key()}: ${String(it.value())}" }}")
+
+                val orderRequestJson = try {
+                    jacksonObjectMapper().apply {
+                        configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
+                    }.readValue(orderRequest, OrderRequest::class.java)
+                } catch(e: Exception) {
+                    throw e
+                }
+
+                processPlaceOrderMessage(orderRequestJson)
+            }
+            CANCEL_ORDER_TOPIC -> {
+                val cancellationRequest = record.value()
+
+                println("[$SERVICE_NAME] Received message on topic $CANCEL_ORDER_TOPIC - $cancellationRequest")
+
+                val orderIdObject = try {
+                    jacksonObjectMapper().apply {
+                        configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
+                    }.readValue(cancellationRequest, OrderId::class.java)
+                } catch(e: Exception) {
+                    throw e
+                }
+
+                processCancellation(orderIdObject)
+            }
         }
-        processMessage(orderRequestJson)
+
         ack.acknowledge()
     }
 
-    private fun processMessage(orderRequest: OrderRequest) {
+    private fun processPlaceOrderMessage(orderRequest: OrderRequest) {
         sendMessageOnProcessOrderTopic(orderRequest)
-        sendMessageOnNotificationTopic()
+        val message = """{"message": "Order processed successfully", "type": "$NOTIFICATION_TYPE_ORDER_PLACED"}"""
+        sendMessageOnNotificationTopic(message)
+    }
+
+    private fun processCancellation(orderIdObject: OrderId) {
+        sendMessageOnProcessCancellationTopic(orderIdObject)
+        val notificationMessage = """{"message": "Order cancelled successfully", "type": "$NOTIFICATION_TYPE_ORDER_CANCELLED"}"""
+        sendMessageOnNotificationTopic(notificationMessage)
+    }
+
+    private fun sendMessageOnProcessCancellationTopic(orderIdObject: OrderId) {
+        val reference = 10
+        val cancellationMessage = """{"reference": $reference, "status": "$CANCELLATION_COMPLETED"}"""
+
+        println("[$SERVICE_NAME] Publishing a message on $PROCESS_CANCELLATION_TOPIC topic: $cancellationMessage")
+        kafkaTemplate.send(PROCESS_CANCELLATION_TOPIC, cancellationMessage)
     }
 
     private fun sendMessageOnProcessOrderTopic(orderRequest: OrderRequest) {
@@ -56,10 +104,9 @@ class OrderService(
         kafkaTemplate.send(PROCESS_ORDER_TOPIC, taskMessage)
     }
 
-    private fun sendMessageOnNotificationTopic() {
-        val taskMessage = """{"message": "Order processed successfully", "type": "$NOTIFICATION_TYPE_ORDER_PLACED"}"""
-        println("[$SERVICE_NAME] Publishing a message on $NOTIFICATION_TOPIC topic: $taskMessage")
-        kafkaTemplate.send(NOTIFICATION_TOPIC, taskMessage)
+    private fun sendMessageOnNotificationTopic(message: String) {
+        println("[$SERVICE_NAME] Publishing a message on $NOTIFICATION_TOPIC topic: $message")
+        kafkaTemplate.send(NOTIFICATION_TOPIC, message)
     }
 }
 
@@ -73,4 +120,13 @@ data class OrderItem(
     val name: String,
     val quantity: Int,
     val price: BigDecimal
+)
+
+data class OrderId(
+    val id: Int
+)
+
+data class CancellationReference(
+    val reference: Int,
+    val status: String
 )
